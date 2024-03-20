69276d01ab19e0b921a49e86e3c763c60a9e51c0
joserodrigoSP <jrodris2005@gmail.com>
2024-03-19 20:55:59 -0600

    Se inicia el proyecto

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿class Program{
+static void Main(string[] args){
+
+int resp = 0 ;
+
+
+Console.WriteLine("¿Qué tipo de leche quisiera agregar?");
+Console.WriteLine("1) Sin leche (agua)");
+Console.WriteLine("2) Leche deslactosada");
+Console.WriteLine("3) Leche entera");
+Console.WriteLine("4) Leche de soya");
+int.TryParse(Console.ReadLine(), out resp);
+
+
+
+}
+}

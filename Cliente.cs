d0a3f83922cba855a5cb459a050ba42baf07974e
joserodrigoSP <jrodris2005@gmail.com>
2024-03-26 23:59:45 -0600

    Avances
    agregué progra a la clase program, mañana sigo con el resto del main.

--- Cliente.cs
+++ Cliente.cs
@@ -9,7 +9,25 @@ nombre =nom ;
 Cnit= cn;
 }
 
-
+public void definirNombre(string nomb){
+nombre = nomb; 
+}
+public void cambiarEstadoNit(){
+    Cnit = !Cnit;
+}
+public string MostrarNit(){
+    if(Cnit == true){
+        return "El Nit es: "+NIT;
+    }else{
+        return "CF";
+    }
+}
+public void definirNit(string n){
+NIT= n;
+}
+public string MostrarNombre(){
+    return nombre;
+}
 
 
 
--- Program.cs
+++ Program.cs
@@ -1,11 +1,44 @@
 ﻿class Program{
 static void Main(string[] args){
 Licuado objlicuado = new Licuado();
+Cliente objcliente = new Cliente();
 int opcion=0;
+int resp;
+string tipoazucar =""; 
+int cdaz=0;
 Console.WriteLine("Qué acción desea realizar \n 1) Elegir azucar \n 2) Elegir Leche \n 3) Agrandar el pedido \n 4) Agregar datos del cliente \n 5) Finalizar pedido");
 int.TryParse(Console.ReadLine(), out opcion);
 switch(opcion){
 case 1:
+
+Console.WriteLine("Qué tipo de azucar le gustaria agregar: \n 1) Azucar blanca \n 2) Azucar morena \n 3) Suplemento \n 4) No agregar");
+int respp; int.TryParse(Console.ReadLine(), out respp);
+switch(respp){
+case 1:
+tipoazucar = "azucar blanca";
+do{
+Console.WriteLine("Cuantas cucharaditas desea agregar? (no mas de 3)");
+int.TryParse(Console.ReadLine(), out cdaz);
+}while(cdaz>=4);
+break; 
+case 2:
+tipoazucar = "azucar morena";
+do{
+Console.WriteLine("Cuantas cucharaditas desea agregar? (no mas de 3)");
+int.TryParse(Console.ReadLine(), out cdaz);
+}while(cdaz>=4);
+break; 
+case 3:
+tipoazucar = "suplemento";
+do{
+Console.WriteLine("Cuantas cucharaditas desea agregar? (no mas de 3)");
+int.TryParse(Console.ReadLine(), out cdaz);
+}while(cdaz>=4);
+cdaz=0;
+break;
+}
+
+objlicuado.definirAzucar(cdaz, tipoazucar);
 
 break;
 
@@ -16,7 +49,17 @@ case 3:
 
 break;
 case 4:
-
+Console.WriteLine("Ingrese el nombre del cliente: ");
+objcliente.definirNombre(Console.ReadLine());
+Console.WriteLine("¿Desea ingresar NIT? 1) SI   2) NO");
+int.TryParse(Console.ReadLine(), out resp);
+if(resp==1){
+objcliente.cambiarEstadoNit();
+Console.WriteLine("Cual es el NIT del cliente?");
+objcliente.definirNit(Console.ReadLine());
+}else{
+Console.WriteLine("CF");
+}
 break;
 case 5:
 
